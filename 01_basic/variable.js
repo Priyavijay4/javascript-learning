42f87408a3d114bfb0e93db1010697cca5a6d308
Priyavijay4 <141993470+Priyavijay4@users.noreply.github.com>
2023-08-16 10:51:01 +0000

    variable and datatypes

---
+++ 01_basic/variable.js
@@ -0,0 +1,13 @@
+const accountId = 1222211;
+let accountEmail = "priya@gmail.com";
+var accountPassword = "123456";
+accountCity = "jaipur";
+let accountState;
+
+accountEmail = "p1@gmail.com";
+accountPassword = "123";
+accountCity = "pune";
+
+
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState]);
+
